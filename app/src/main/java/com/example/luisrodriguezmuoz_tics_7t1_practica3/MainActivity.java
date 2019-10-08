28706e9cb51a9e6629d90fb45e137c396591695c
LuisRodriguezMunoz18 <serpicoerum2@github.com>
2019-10-07 20:15:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/luisrodriguezmuoz_tics_7t1_practica3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.luisrodriguezmuoz_tics_7t1_practica3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
